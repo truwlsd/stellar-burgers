adaa821396f8b3da15bea9cea2b417c86bfe3d10
Tr <agaasdasdj1199@gmail.com>
2026-03-13 16:18:17 +0300

    2p

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -1,15 +1,30 @@
-import { FC, memo } from 'react';
+import { FC, memo, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useDispatch } from '../../services/store';
+import { useSelector, useDispatch } from '../../services/store'; // Проверь путь!
 import { addIngredient } from '../../services/slices/constructorSlice';
-
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
+import { TIngredient, TConstructorIngredient } from '@utils-types';
 
 export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
-  ({ ingredient, count }) => {
+  ({ ingredient }) => {
     const location = useLocation();
     const dispatch = useDispatch();
+
+    // Достаем данные конструктора для расчета счетчика
+    const { bun, ingredients } = useSelector(
+      (state) => state.burgerConstructor
+    );
+
+    // Считаем количество (count)
+    const count = useMemo(() => {
+      if (ingredient.type === 'bun') {
+        return bun?._id === ingredient._id ? 2 : 0;
+      }
+      return ingredients.filter(
+        (item: TConstructorIngredient) => item._id === ingredient._id
+      ).length;
+    }, [bun, ingredients, ingredient]);
 
     const handleAdd = () => {
       dispatch(addIngredient(ingredient));
